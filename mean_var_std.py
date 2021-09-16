240f7e1d984639ad3a33d1961017bef6b0ca556f
Brian Marquis <rbmarquis.dev@gmail.com>
2021-09-15 20:56:07 -0500

    define calculate func

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,48 @@
 import numpy as np
 
-def calculate(list):
+def calculate(lst):
 
+    if len(lst) < 9:
+        raise ValueError("List must contain nine numbers.")
 
+    # Convert the list into a 3 x 3 Numpy array
+    arr = np.array(lst).reshape(3, 3)
 
+    # Return a dictionary containing the mean, variance, standard deviation, max, min, and sum along both axes and for the flattened matrix. The values in the returned dictionary should be lists and not Numpy arrays.
+    calculations = {
+        'mean': [
+            list(np.mean(arr, axis=0)), 
+            list(np.mean(arr, axis=1)), 
+            np.mean(arr.flatten())
+        ],
+        'variance': [
+            list(np.var(arr, axis=0)), 
+            list(np.var(arr, axis=1)), 
+            np.var(arr.flatten())
+        ],
+        'standard deviation': [
+            list(np.std(arr, axis=0)), 
+            list(np.std(arr, axis=1)), 
+            np.std(arr.flatten())
+        ],
+        'max': [
+            list(np.max(arr, axis=0)), 
+            list(np.max(arr, axis=1)), 
+            np.max(arr.flatten())
+        ],
+        'min': [
+            list(np.min(arr, axis=0)), 
+            list(np.min(arr, axis=1)), 
+            np.min(arr.flatten())
+        ],
+        'sum': [
+            list(np.sum(arr, axis=0)), 
+            list(np.sum(arr, axis=1)), 
+            np.sum(arr.flatten())
+        ]
+    }
 
     return calculations
+
+
+# print(calculate([0,1,2,3,4,5,6,7,8]))
